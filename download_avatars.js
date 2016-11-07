b7ac80f03f6b2e66d2de99ad8fa36206213211a0
Graham Lee <wgin.lee@gmail.com>
2016-11-07 22:29:46 +0000

    request url string

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,17 @@
+var args = process.argv.slice(2);
+
 var request = require('request');
+
+const GITHUB_USER = "wginlee";
+const GITHUB_TOKEN = "40296713ef37d0aae3a6d706d7c45010938b3aaf";
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  // console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
