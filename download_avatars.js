5001a6687849bdb74498560c27310406f5c3272c
Graham Lee <wgin.lee@gmail.com>
2016-11-08 00:21:32 +0000

    print all avatar_urls (for now)

--- download_avatars.js
+++ download_avatars.js
@@ -10,10 +10,14 @@ const GITHUB_TOKEN = "40296713ef37d0aae3a6d706d7c45010938b3aaf";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function githubUsername(){
+  return args[0];
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  // console.log(requestURL);
+  // var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL);
 
   var options = {
     url: requestURL,
@@ -22,23 +26,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request.get(options)
-    .on('error', function (err) {
-      throw err;
-    })
-    .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode);
-          console.log('Response Status Message: ', response.statusMessage);
-          console.log('Content Type: ', response.headers['content-type']);
-          // console.log('Downloading image...');
-       })
-    .on('end', function(response){
-          console.log('Download complete.');
-       })
-    // .pipe(fs.createWriteStream('./future.jpg'));
+  request.get(options, function (error, response, body){
+    // console.log(response.headers['content-type']);
+    const responseData = JSON.parse(body);
+    // console.log(Object.keys(responseData[0]));
+    responseData.forEach( (element) => {
+      console.log(element.avatar_url);
+    });
+    // cb();
+
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+// function putAvatarURLs(contributors){
+//   contributors.forEach(function (contributor){
+//     console.log("do stuff to me");
+//   });
+// }
+
+// getRepoContributors("jquery", "jquery", putAvatarURLs);
